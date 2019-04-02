2659b82b86866ef38d55b0e3a9af0d3c4ae45ae1
Derek Hackett <derek.hackett@unifyconsulting.com>
2019-04-02 11:25:34 -0500

    Update HomeNetworx.vue
    Change Name pased on feedback.

--- src/components/HomeNetworx.vue
+++ src/components/HomeNetworx.vue
@@ -1,7 +1,7 @@
 <template>
       <v-container fluid fill-height class="Home-Networx" style="max-height: 100vh;">
         <v-layout align-start justify-end column pa-5>
-            <div class="display-3 font-weight-black black--text text-xs-center">Networx</div>
+            <div class="display-3 font-weight-black black--text text-xs-center">Network Sioux Falls</div>
             <div class="display-2 font-weight-black black--text text-xs-center">Advances in A.I.</div>
             <div class="display-1 font-weight-black light-blue--text text--darken-4 text-xs-center mb-3">Sioux Falls Tech Mingle</div>
             <div class="headline font-weight-bold grey--text text--lighten-1 text-xs-center">April 24th - Zeal Center for Entrepreneurship</div>
